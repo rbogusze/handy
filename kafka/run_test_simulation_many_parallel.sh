e1f16c78bc4b644605035c10a0d999d691e596de
Remigiusz Boguszewicz <rboguszewicz@rboguszewicz-devdocker-2zdv-03.dev-i0b.pc1.lon1.dev.booking.com>
2023-05-26 13:59:11 +0200

    a

--- kafka/run_test_simulation_many_parallel.sh
+++ kafka/run_test_simulation_many_parallel.sh
@@ -4,6 +4,9 @@ cd ${KAFKA_BIN}
 
 # number of parallel executions, by default 2 if not set with V_EXECUTE variable
 [ -z "$V_EXECUTE" ] && V_EXECUTE=2
+[ -z "$V_THROUGHPUT" ] && V_THROUGHPUT="-1"
+[ -z "$V_NUMRECORDS" ] && V_NUMRECORDS=5900000
+
 
 # range, topic number
 CURRENT=0
@@ -24,7 +27,7 @@ do
     #./kafka-producer-perf-test.sh --topic ${TOPIC_NAME}${CURRENT}  --throughput 30000 --num-records 9900000 --record-size 1024 --producer-props acks=all bootstrap.servers=${BOOTSTRAP_SERVERS}
 
     # no limit
-    ./kafka-producer-perf-test.sh --topic ${TOPIC_NAME}${CURRENT}  --throughput -1 --num-records 5900000 --record-size 1024 --producer-props acks=all bootstrap.servers=${BOOTSTRAP_SERVERS} | awk -v var="$CURRENT" '{print var": " $0 }' &
+    ./kafka-producer-perf-test.sh --topic ${TOPIC_NAME}${CURRENT}  --throughput ${V_THROUGHPUT} --num-records ${V_NUMRECORDS} --record-size 1024 --producer-props acks=all bootstrap.servers=${BOOTSTRAP_SERVERS} | awk -v var="$CURRENT" '{print var": " $0 }' &
     sleep 1
     V_EXECUTE_COUNT=`expr $V_EXECUTE_COUNT + 1`
   done
