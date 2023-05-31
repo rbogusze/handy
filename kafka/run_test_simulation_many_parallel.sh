c7ce3475aeaee4da9152d6b4b89b7b4277e215c2
Remigiusz Boguszewicz <rboguszewicz@kafka>
2023-05-31 23:00:05 +0200

    a

--- kafka/run_test_simulation_many_parallel.sh
+++ kafka/run_test_simulation_many_parallel.sh
@@ -7,7 +7,10 @@ cd ${KAFKA_BIN}
 [ -z "$V_THROUGHPUT" ] && V_THROUGHPUT="-1"
 [ -z "$V_NUMRECORDS" ] && V_NUMRECORDS=5900000
 [ -z "$V_RECORDSIZE" ] && V_RECORDSIZE=1024
+[ -z "$V_ACKS" ] && V_ACKS="all"
 
+echo "Running with the following parameters"
+env | grep V_
 
 # range, topic number
 CURRENT=0
@@ -21,6 +24,8 @@ do
     echo "running $V_EXECUTE_COUNT"
     # introduce some random stuff
     CURRENT=$[ ( $RANDOM % $END )  + 1 ]
+    # just using one topic
+    CURRENT=1
 
     echo "run test ${CURRENT}"
 
@@ -28,7 +33,7 @@ do
     #./kafka-producer-perf-test.sh --topic ${TOPIC_NAME}${CURRENT}  --throughput 30000 --num-records 9900000 --record-size 1024 --producer-props acks=all bootstrap.servers=${BOOTSTRAP_SERVERS}
 
     # no limit
-    ./kafka-producer-perf-test.sh --topic ${TOPIC_NAME}${CURRENT}  --throughput ${V_THROUGHPUT} --num-records ${V_NUMRECORDS} --record-size ${V_RECORDSIZE} --producer-props acks=all bootstrap.servers=${BOOTSTRAP_SERVERS} | awk -v var="$CURRENT" '{print var": " $0 }' &
+    ./kafka-producer-perf-test.sh --topic ${TOPIC_NAME}${CURRENT}  --throughput ${V_THROUGHPUT} --num-records ${V_NUMRECORDS} --record-size ${V_RECORDSIZE} --producer-props acks=${V_ACKS} bootstrap.servers=${BOOTSTRAP_SERVERS} | awk -v var="$CURRENT" '{print var": " $0 }' &
     sleep 1
     V_EXECUTE_COUNT=`expr $V_EXECUTE_COUNT + 1`
   done
